c97f36cb0d5f53ac11fdfa05dceb4fe9cbb30770
UIS <veloteh@outlook.com>
2019-02-24 22:20:27 +0300

    24feb_2

--- src/ru/avalon/java/ocpjp/labs/actions/FileCopyAction.java
+++ src/ru/avalon/java/ocpjp/labs/actions/FileCopyAction.java
@@ -1,5 +1,9 @@
 package ru.avalon.java.ocpjp.labs.actions;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Действие, которое копирует файлы в пределах дискового
  * пространства.
@@ -13,6 +17,26 @@ public class FileCopyAction implements Action {
         /*
          * TODO №2 Реализуйте метод run класса FileCopyAction
          */
+        ArrayList<String> newnames = new ArrayList<>();
+        File file = new File("c:\\Users\\UIS\\Desktop\\Avalon\\lab-1\\lab-1\\src\\ru\\avalon\\java\\ocpjp\\labs\\resources\\first-names.txt");
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                newnames.add(scanner.nextLine()); // записал в все строки из файла в ArrayList
+                Writer writer = null;
+                writer = new FileWriter("c:\\Users\\UIS\\Desktop\\Avalon\\lab-1\\lab-1\\src\\ru\\avalon\\java\\ocpjp\\labs\\resources\\newnames.txt");
+                for (String line : newnames) {
+                    writer.write(line);
+                    writer.write(System.getProperty(" "));
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         throw new UnsupportedOperationException("Not implemented yet!");
     }
 
